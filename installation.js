48ef3aa83a65349a637376a2b798aa82146a0ccd
James Kotzian <jkotzian@umich.edu>
2015-11-19 19:22:03 -0500

    Deleted installation file

--- installation.js
+++
@@ -1,9 +0,0 @@
-var hello = {
-    world: function(str, callback) {
-        cordova.exec(callback, function(err) {
-        	callback('Nothing to hello.');
-    	}, "Hello", "hello", [str]);
-    }
-}
-
-module.exports = hello;
