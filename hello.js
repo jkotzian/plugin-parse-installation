94e429ea7a1599f32902db8c9d984b37f2bc5776
James Kotzian <jkotzian@umich.edu>
2015-11-20 17:46:30 -0500

    renamed something in js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
-var hello = {
+var installation = {
     setUsernameForInstallationId: function(str, callback) {
     	cordova.exec(callback, function(err) {callback('Nothing here!');}, "MyInstallation", "hello", [str]);
     }
 }
 
-module.exports = hello;
+module.exports = installation;
