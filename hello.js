0ef826fb695f303406ef4ca59b79978a9e25b083
James Kotzian <jkotzian@umich.edu>
2015-11-20 18:23:45 -0500

    Reverted completely

--- hello.js
+++ hello.js
@@ -1,6 +1,9 @@
 var hello = {
+    getInstallationIdForUsername: function(str, callback) {
+        cordova.exec(callback, function(err) {callback('Nothing to hello.');}, "Hello", "hello", [str]);
+    },
     setUsernameForInstallationId: function(str, callback) {
-    	cordova.exec(callback, function(err) {callback('Nothing here!');}, "Hello", "hello", [str]);
+    	cordova.exec(callback, function(err) {callback('Nothing here!');}, "Hello", "hey", [str]);
     }
 }
 
