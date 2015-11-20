103c0abf0fd9da1610af3046c9cefc209ea883b3
James Kotzian <jkotzian@umich.edu>
2015-11-19 19:09:18 -0500

    Added comma

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 var hello = {
     world: function(str, callback) {
         cordova.exec(callback, function(err) {callback('Nothing to hello.');}, "Hello", "hello", [str]);
-    }
+    },
     setUserWithInstallation: function(str, callback) {
     	cordova.exec(callback, function(err) {callback('Nothing here!');}, "Hello", "hey", [str]);
     }
